1504abe82122494e2bfdf526ee2f7c1b4acae0c2
pukhraj saini <pukhraj.saini@mobilecoderz.com>
2023-01-05 15:19:59 +0530

    initial commit

---
+++ src/app/modules/image/image.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectImageComponent } from './select-image/select-image.component';
+
+
+
+@NgModule({
+  declarations: [
+    SelectImageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImageModule { }
