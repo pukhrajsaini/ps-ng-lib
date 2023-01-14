cedfc83269fd27378dc5b917b74eff6d0eead57b
pukhraj saini <pukhraj.saini@mobilecoderz.com>
2023-01-14 15:33:40 +0530

    configuration styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SELECT_IMAGE_ROUTE } from './constants/route.constant';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: SELECT_IMAGE_ROUTE.path,
+    loadChildren: () => import('./pages/test-select-image/test-select-image.module').then(m => m.TestSelectImageModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
