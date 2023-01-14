9ed266f44db06a340da22805bc72bbf6fefaaefd
pukhraj saini <pukhraj.saini@mobilecoderz.com>
2023-01-14 15:09:28 +0530

    set up routes and pages

---
+++ src/app/constants/route.constant.ts
@@ -0,0 +1,6 @@
+export const SELECT_IMAGE_ROUTE = {
+    path: 'select-image',
+    get url(): string {
+        return `/${this.path}`
+    }
+}
---
+++ src/app/pages/test-select-image/test-select-image.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewComponent } from './components/view/view.component';
+
+
+
+@NgModule({
+  declarations: [
+    ViewComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TestSelectImageModule { }
